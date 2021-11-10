539d88db49b081a988fd8535428cb3fcb9764283
Carl Johnson <me@carlmjohnson.net>
2021-11-10 10:33:13 -0500

    Tests: Use blackbox testing

--- deque_test.go
+++ deque_test.go
@@ -1,17 +1,23 @@
-package deque
+package deque_test
 
 import (
 	"fmt"
 	"sort"
+
+	"github.com/carlmjohnson/deque"
 )
 
 func ExampleDeque() {
-	d := Of(9, 8, 7, 6)
-	sort.Sort(Sortable[int]{d})
+	// Make a new deque
+	d := deque.Of(9, 8, 7, 6)
+	// Sort it
+	sort.Sort(deque.Sortable[int]{d})
+	// Add 5, 4, 3, 2, 1 to the front
 	for i := 5; i > 0; i-- {
 		d.PushHead(i)
 	}
 	fmt.Println(d)
+	// Now pop items off the tail
 	for {
 		n, ok := d.PopTail()
 		if !ok {
